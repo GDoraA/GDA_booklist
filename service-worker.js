695508481aa1c44bd1b91ce2895a9436d04c9390
GDoraA <galbacsdori@gmail.com>
2025-11-24 23:14:02 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,21 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-  caches.open("gda-cache").then(cache => {
-    return cache.addAll([
-      "/GDA_booklist/index.html",
-      "/GDA_booklist/styles.css",
-      "/GDA_booklist/app.js",
-      "/GDA_booklist/assets/icon-192.png",
-      "/GDA_booklist/assets/icon-512.png"
-    ]);
-  });
-
+    caches.open("gda-cache").then(cache => {
+      return cache.addAll([
+        "/GDA_booklist/index.html",
+        "/GDA_booklist/styles.css",
+        "/GDA_booklist/app.js",
+        "/GDA_booklist/manifest.json",
+        "/GDA_booklist/assets/icon-192.png",
+        "/GDA_booklist/assets/icon-512.png"
+      ]);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", e => {
